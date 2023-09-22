c668d5edef9d015be7a9ea874f6be8df0b2e6dc9
alex <29205589+aquino-a@users.noreply.github.com>
2023-09-22 11:01:44 +0900

    add view and view model

--- LcrSimulator.Core/MainWindowViewModel.cs
+++ LcrSimulator.Core/MainWindowViewModel.cs
@@ -1,40 +1,79 @@
-﻿using System.Windows.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
 
 namespace LcrSimulator.Core
 {
-    public class MainWindowViewModel
+    public partial class MainWindowViewModel : ObservableObject
     {
+        [ObservableProperty]
+        public SimulationResult _currentResult;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(GameCount))]
+        [NotifyPropertyChangedFor(nameof(PlayerCount))]
+        public Setting _currentSetting;
+
         private readonly ISimulator _simulator;
+        private CancellationTokenSource _cancelSource;
 
         public MainWindowViewModel(ISimulator simulator)
         {
             _simulator = simulator;
+            PlayCommand = new RelayCommand(Play, () => _currentSetting != null && _cancelSource == null);
+            CancelCommand = new RelayCommand(Cancel, () => _cancelSource != null && _cancelSource.Token.CanBeCanceled);
         }
 
-        public record Setting(int playerCount, int gameCount);
-
-        public ICommand CancelCommand { get; set; }
-
-        public SimulationResult CurrentResult { get; set; }
-
-        public Setting CurrentSetting { get; set; }
-
+        public ICommand CancelCommand { get; private set; }
         public int GameCount { get; set; }
 
-        public ICommand PlayCommand { get; set; }
+        public ICommand PlayCommand { get; private set; }
 
         public int PlayerCount { get; set; }
 
         public List<Setting> Presets { get; set; } = new List<Setting>()
         {
-            new Setting(3, 100),
-            new Setting(4, 100),
-            new Setting(5, 100),
-            new Setting(5, 1_000),
-            new Setting(5, 10_000),
-            new Setting(5, 100_000),
-            new Setting(6, 100),
-            new Setting(7, 100),
+            new Setting{ GameCount = 3, PlayerCount = 100 },
+            new Setting{ GameCount = 4, PlayerCount = 100 },
+            new Setting{ GameCount = 5, PlayerCount = 100 },
+            new Setting{ GameCount = 5, PlayerCount = 1_000 },
+            new Setting{ GameCount = 5, PlayerCount = 10_000 },
+            new Setting{ GameCount = 5, PlayerCount = 100_000 },
+            new Setting{ GameCount = 6, PlayerCount = 100 },
+            new Setting{ GameCount = 7, PlayerCount = 100 },
         };
+
+        private void Cancel()
+        {
+            if (_cancelSource == null)
+            {
+                return;
+            }
+
+            _cancelSource.Cancel();
+        }
+
+        private async void Play()
+        {
+            if (_cancelSource != null)
+            {
+                return;
+            }
+
+            try
+            {
+                _cancelSource = new CancellationTokenSource();
+                var result = await Task.Factory.StartNew(() =>
+                {
+                    return _simulator.Simulate(CurrentSetting.PlayerCount, CurrentSetting.GameCount);
+                }, _cancelSource.Token);
+
+                CurrentResult = result;
+            }
+            finally
+            {
+                _cancelSource = null;
+            }
+        }
     }
 }
---
+++ LcrSimulator.Core/Setting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LcrSimulator.Core
+{
+    public class Setting
+    {
+        public int PlayerCount { get; set; }
+        public int GameCount { get; set; }
+        public string Display { get => $"{PlayerCount} Players x {GameCount} games"; }
+    }
+}
